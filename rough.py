4e622642eaba7f5717c5aa103979b274af64842d
99003729 <pushkar.antony@ltts.com>
2021-02-23 09:31:14 +0530

    23feb tues morning commit

--- rough.py
+++ rough.py
@@ -1,5 +1,9 @@
-s=input()
-s = list(s.split(' '))
-print(s)
-for i in s:
-    print(i)
+n=int(input())
+c=0
+s=set()
+while n>c:
+    name=input()
+    s.add(name)
+    c+=1
+count=len(s)
+print(count)
