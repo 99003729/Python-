ae4108451d4f24f97cc11e1114fe149d030d4034
99003729 <pushkar.antony@ltts.com>
2021-02-24 16:09:51 +0530

    update for Python miniproject commited by pushkar

--- Mini_Project_Python/99003729.py
+++ Mini_Project_Python/99003729.py
@@ -11,6 +11,7 @@ import re
 
 my_file = open("input.txt", "rt")  # opening the input text file from directory
 cont = my_file.read()  # reading the input text file
+cont_new = cont.split()
 n = int(input("\n Enter the number of keywords you want to search for: "))  # asking user for no of keywords.
 c = 0  # initialising the count as 0
 while n > c:  # entering a while loop
@@ -19,8 +20,10 @@ while n > c:  # entering a while loop
     out_file = open(file_name, "a+")
     pattern = re.findall(keyword, cont, re.M | re.I)
     keyword_count = str(len(pattern))
-    for i in pattern:
-        out_file.write(i + "\n")
-    out_file.write("\n The word count for the given keyword is " + keyword_count)
+    for i in range(len(cont_new)):
+        if re.match(keyword, cont_new[i], re.M | re.I):
+            out_file.write(" the keyword: " + keyword + " was found on this line -->  " +
+                           cont_new[i-1] + " " + cont_new[i] + " " + cont_new[i+1] + "\n")
+    out_file.write("\n The word count for the given keyword is: " + keyword_count)
     out_file.close()
     c += 1
