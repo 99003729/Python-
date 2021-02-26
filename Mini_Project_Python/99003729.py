ab6e160f1d74805a20818728eadb86cfe16344cc
99003729 <pushkar.antony@ltts.com>
2021-02-26 09:31:49 +0530

    added comments to the whole project - commited by pushkar

--- Mini_Project_Python/99003729.py
+++ Mini_Project_Python/99003729.py
@@ -12,7 +12,14 @@ Date of Creation: 23/02/2021
 """
 import re
 
-""" The Parent class is defined below. """
+""" The Parent class is defined below.(Using Classes)
+
+ line 28: opening the input text file from directory
+ line 29: reading the input text file
+ line 30: using regex split the read input file free of
+ special characters and whitespaces
+
+"""
 
 
 class SearchKeywords:
@@ -23,7 +30,23 @@ class SearchKeywords:
         self.cont_new = re.split(r"\W+", self.cont)
 
 
-""" A sub class inherits from the parent class below. """
+""" A sub class inherits from the parent class below. (Using Inheritance)
+
+ line 54: initialising count for while loop as 0
+ line 55: asking user for no of keywords.
+ line 56: entering a while loop
+ line 57: asking user for keyword input
+ line 58: creating the output file name from user input
+ line 59: output file created using append+(append + read)
+ line 60: using findall regex to create a pattern for the keyword
+ line 61: finding the length of the pattern and hence the keyword count
+ line 62: starting a 'for' loop to search index by index inside cont_new
+ line 63: checking if the keyword matches with any word inside cont_new
+ line 64: writing the keywords
+ line 70: writing the keyword count
+ line 71: closing the file
+ line 73: incrementing the count by 1
+"""
 
 
 class InputKeyword(SearchKeywords):
@@ -50,8 +73,15 @@ class InputKeyword(SearchKeywords):
             c += 1
 
 
-""" Object definition and calling"""
+""" Object definition and calling
+
+line 83: defining an object keyword
+line 84: calling main program using object.method
+"""
 
 
 object_keyword = InputKeyword()
 object_keyword.find_keyword()
+
+
+""" END OF PROGRAM"""
